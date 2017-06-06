7d40d82d1df7d2abd8a387ddb902f2a1dc0326a8
nelsonic <contact.nelsonic+github@gmail.com>
2017-06-06 16:03:19 +0100

    fix syntax error (missing comma) in config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :api, Api.Web.Endpoint,
   on_init: {Api.Web.Endpoint, :load_from_system_env, []},
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: System.get_env("HEROKU_URL") , port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Api.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:phoenix, "~> 1.3.0-rc", },
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
