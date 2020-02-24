4c1907d50ac8e79fe2b40575e2605ef52896596d
nelsonic <nelson+github@dwyl.io>
2020-02-24 19:05:17 +0000

    remove _bak/ folder with Ye Olde files

--- _bak/handlers/anonymous.js
+++
@@ -1,6 +0,0 @@
-var JWT = require('../lib/auth_jwt_sign.js');
-module.exports = function handler(req, reply) {
-  JWT(req, function(token, esres){
-    return reply(esres).header("Authorization", token);
-  }); // Async
-}
