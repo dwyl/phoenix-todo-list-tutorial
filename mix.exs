19100c0a544b3a2d692fd79736e1747db4dc387f
nelsonic <nelson+github@dwyl.io>
2020-02-24 19:18:41 +0000

    add excoveralls mix.exs to run coverage report

--- mix.exs
+++ mix.exs
@@ -10,7 +10,10 @@ defmodule App.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test,
+                    "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -42,7 +45,10 @@ defmodule App.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      # Test Code Coverage:
+      {:excoveralls, "~> 0.12.2", only: :test},
     ]
   end
 
