f66184b58b7dd1ef593680e7a1a446247909cae7
nelsonic <nelson+github@dwyl.io>
2020-05-21 14:53:24 +0100

    add resources "/items", ItemController to router.ex

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -17,6 +17,7 @@ defmodule AppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/items", ItemController
   end
 
   # Other scopes may use custom stacks.
