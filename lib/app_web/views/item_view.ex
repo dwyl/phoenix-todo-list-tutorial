9147b0643549a988c789d35c70ab08b9229020bd
nelsonic <nelson+github@dwyl.io>
2020-05-23 10:35:28 +0100

    add complete/1 and checked/1 view functions to lib/app_web/views/item_view.ex step 3.3 of #36

--- lib/app_web/views/item_view.ex
+++ lib/app_web/views/item_view.ex
@@ -1,3 +1,20 @@
 defmodule AppWeb.ItemView do
   use AppWeb, :view
+
+  # add class "completed" to a list item if item.status=1
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
+
+  # add checked="true" to input if item.status=1
+  def checked(item) do
+    case item.status do
+      1 -> "true"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
+
 end
