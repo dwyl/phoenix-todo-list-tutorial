b27b0c9eb63afccf114c3b9af3d9236ded2ea638
nelsonic <nelson+github@dwyl.io>
2020-05-29 02:26:58 +0100

    Step 9.3: Create filter/2 and selected/2 view functions #36

--- lib/app_web/views/item_view.ex
+++ lib/app_web/views/item_view.ex
@@ -23,4 +23,20 @@ defmodule AppWeb.ItemView do
   def remaining_items(items) do
     Enum.filter(items, fn i -> i.status == 0 end) |> Enum.count()
   end
+
+  # filter the items based on the filter string
+  def filter(items, str) do
+    case str do
+      "all" -> items
+      "active" -> Enum.filter(items, fn i -> i.status == 0 end)
+      "completed" -> Enum.filter(items, fn i -> i.status == 1 end)
+    end
+  end
+
+  def selected(filter, str) do
+    case filter == str do
+      true -> "selected"
+      false -> ""
+    end
+  end
 end
