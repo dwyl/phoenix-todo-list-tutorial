fcdc3a64a31e6608876d4c290a0424e31f2603f4
nelsonic <nelson+github@dwyl.io>
2020-05-29 01:23:45 +0100

    Step 9.1 create /:filter route #36

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -19,7 +19,7 @@ defmodule AppWeb.Router do
     get "/", PageController, :index
     resources "/items", ItemController
     get "/items/toggle/:id", ItemController, :toggle
-    get "/items/:filter", ItemController, :index
+    get "/:filter", ItemController, :index
   end
 
   # Other scopes may use custom stacks.
