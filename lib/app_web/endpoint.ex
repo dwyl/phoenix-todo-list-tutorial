fc01d01b80cec541749551feea6ac82195a95198
nelsonic <nelson+github@dwyl.com>
2021-09-15 00:14:48 +0100

    update config/prod.exs & prod.secret.exs for Heroku see: https://github.com/dwyl/phoenix-todo-list-tutorial/issues/47#issuecomment-919576012

--- lib/app_web/endpoint.ex
+++ lib/app_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule AppWeb.Endpoint do
   ]
 
   socket "/socket", AppWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
